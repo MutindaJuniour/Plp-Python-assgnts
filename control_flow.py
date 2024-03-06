39f52e877be3f69ba7f363ec8487794df4a2eea2
MutindaJuniour <mutindasamuel665@gmail.com>
2024-03-06 13:33:26 +0300

    my wk 1 assgnt

---
+++ control_flow.py
@@ -0,0 +1,26 @@
+#words = ["one","two","three","4"]
+#for x in words:
+ #print(x)
+
+#while loop
+#i = 1
+#while i < 6:
+    #print(i)
+    #i += 2
+
+#Jump statements
+#while x <10:
+    #print("x:",x)
+    #if x == 6:
+        #print("Breaking...."
+         #break
+     #x += 1
+
+
+
+
+for letter in "Powerlearn Project": # continue when the letter is 'h'
+    if letter == "L": continue
+print("current letter :",letter)
+
+
---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Asking user for their name and storing it in a variable called "name"
+name = input("Enter your name here? ")
+
+# Asking user for their age and storing it in a variable called "age"
+age = input("How old are you? ")
+
+# Asking user for their location and storing it in a variable called "location"
+location = input("Where are you located? ")
+
+# Printing out a personalized message using the user's name, age, and location
+print(f"Hello {name}, you are {age} years old and live in {location}.")
